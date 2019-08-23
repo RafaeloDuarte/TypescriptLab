6a0d15e55db368000edcc57bbd8d5fbc77392296
Rafaelo Duarte <rafaeloduarte@gmail.com>
2019-08-23 16:16:48 -0300

    Inserindo arquivo tsconfig.json e iniciando os estudos de tipos de variáveis

---
+++ src/variables-declarations/variables-declarations.js
@@ -0,0 +1,8 @@
+function initGame(init) {
+    var name = 'Rafaras';
+    if (init) {
+        var city = ' São Paulo HJ.';
+    }
+    console.log(name + " est\u00E1 em " + city);
+}
+initGame(true);
---
+++ src/variables-declarations/variables-declarations.ts
@@ -0,0 +1,12 @@
+function initGame (init:boolean){
+    var name = 'Rafaras';
+
+    if(init){
+        var city = ' São Paulo HJ.';
+    }
+
+    console.log(`${name} está em ${city}`);
+
+}
+
+initGame(true);
